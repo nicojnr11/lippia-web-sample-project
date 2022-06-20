679251518ad5f23fb902a798fd9872187ec1d806
unknown <nicojnr11@gmail.com>
2022-06-20 18:31:05 -0300

    update

--- src/main/java/lippia/web/steps/MyAccountSteps.java
+++ src/main/java/lippia/web/steps/MyAccountSteps.java
@@ -1,9 +1,36 @@
 package lippia.web.steps;
 
 import com.crowdar.core.PageSteps;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
 
 public class MyAccountSteps extends PageSteps {
 
+    @Given("Click on My Account link")
+    public void clickOnMyAccountLink(){
+
+    }
+
+    @When("Click on Account details")
+    public void clickOnAccountDetails(){
+
+    }
+
+    @When("Click on Logout button")
+    public void clickOnLogoutButton(){
+
+    }
+
+    @Then("User can view account details where he can change his pasword also")
+    public void usercanviewaccountdetailswherehecanchangehispaswordalso(){
+
+    }
+
+    @Then("On clicking logout,User successfully comes out from the site")
+    public void logOutUser(){
+
+    }
 
 }
--- src/test/resources/Feature/MyAccount.feature
+++ src/test/resources/Feature/MyAccount.feature
@@ -3,15 +3,15 @@ Feature: My Account practice
   Background:
     Given The user naviga to in the page automation
     And Click on My Account Menu
-    When Enter the case changed <username> in username textbox
-    And Enter the case chenged <password> in the password tetxbox
+    When Enter the case changed username in username textbox
+    And Enter the case chenged password in the password tetxbox
     And Now click on login button
 
   Scenario: My Accounts-Account Details
 
     Given Click on My Account link
     When Click on Account details
-    Then User can view account details where he can change his pasword also.
+    Then User can view account details where he can change his pasword also
 
   Scenario: My Accounts-Log-Out
 
